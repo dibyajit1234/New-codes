c07a411a1bbccad58770ab125fb504864b8d7d1d
dibyajit1234 <dibyajitnandi7205@gmail.com>
2024-06-15 10:59:07 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+let things=[
+    {
+        name:"dibyajit",
+        class:23,
+        sec:"v",
+    },
+    {
+        name:"oishi nandi",
+        class:3,
+        sec:"vfds",
+    },
+    {
+        name:"biswajit",
+        class:234,
+        sec:"v4vsa",
+    },
+
+]
+let a =things[0];
+let b=a["name"];
+console.log(b);
+console.log(a);
+// let manus={
+//     name:"dibyajit nandi",
+//     class:9,
+//     sec:"c",
+
+// }
+
+// console.log(manus);
