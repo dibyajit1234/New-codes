c6a781507568efd28f4f01c18f4ce8c3495a0d54
dibyajit1234 <dibyajitnandi7205@gmail.com>
2024-06-29 21:32:35 +0530

    updated

--- quiz app/script.js
+++ quiz app/script.js
@@ -1,8 +1,40 @@
-//const a = "https://opentdb.com/api.php?amount=4";
-//const get=async()=> {
-  //let b=await fetch(a);
-//  console.log(b);
-  //let c=b.json();
- // console.log(c);
-//}
-//get(); 
+const a = "https://opentdb.com/api.php?amount=4";
+// const url="http://api.weatherapi.com/v1/current.json?key=fc71dce7dd2540b2af7150945241506&q=India&aqi=no";
+
+async function get (){
+  let b=await fetch(a);
+  let c=await b.json();
+  return c;
+}
+
+let start = document.querySelector(".start");
+let container=document.querySelector(".container")
+let newer=document.querySelector(".new");
+let butt=document.querySelector(".butt");
+let question = document.querySelector("#question");
+let opt = document.querySelector(".opt");
+
+
+
+
+newer.addEventListener(('click'),()=>{
+  container.classList.remove("hide");
+  butt.classList.remove("hide");
+  start.style.display = "none";
+  get().then((a)=>{
+    console.log(a);
+ for(let i=0;i<4;i++){
+      if(a.results[i].type=="multiple"){
+        question.innerHTML="Q . "+a.results[i].question;
+      }
+    }
+    options_sort();
+
+ })
+})
+
+
+const options_sort=(()=>{
+  let choice = Math.floor(Math.random()*4);
+  console.log(choice);
+})
--- script.js
+++
@@ -1,5 +0,0 @@
-const url="https://opentdb.com/api.php?amount=4";
-const geturl = async()=>{
-  let b=await fetch(url);
-  console.log(b);
-}
