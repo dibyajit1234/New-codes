5a2f3e891b8f48ade78fb550a13f7ccea9c40540
dibyajit1234 <dibyajitnandi7205@gmail.com>
2024-06-30 14:24:49 +0530

    updated

--- quiz app/script.js
+++ quiz app/script.js
@@ -12,9 +12,13 @@ let container=document.querySelector(".container")
 let newer=document.querySelector(".new");
 let butt=document.querySelector(".butt");
 let question = document.querySelector("#question");
-let opt = document.querySelector(".opt");
-
-
+let op1=document.querySelector("#op1");
+let op2=document.querySelector("#op2");
+let op3=document.querySelector("#op3");
+let op4=document.querySelector("#op4");
+let option=document.querySelectorAll(".opt");
+let submit=document.querySelector("#submit");
+let next=document.querySelector("#next");
 
 
 newer.addEventListener(('click'),()=>{
@@ -23,18 +27,95 @@ newer.addEventListener(('click'),()=>{
   start.style.display = "none";
   get().then((a)=>{
     console.log(a);
+    let n;
+  
  for(let i=0;i<4;i++){
-      if(a.results[i].type=="multiple"){
+      if(a.results[i].type==="multiple"){
         question.innerHTML="Q . "+a.results[i].question;
       }
+
+      n=i;
     }
-    options_sort();
+
+    option_sort(a.results[n].incorrect_answers[0],a.results[n].incorrect_answers[1],a.results[n].incorrect_answers[2],a.results[n].correct_answer,op1,op2,op3,op4,n);
+
 
  })
 })
 
-
-const options_sort=(()=>{
-  let choice = Math.floor(Math.random()*4);
-  console.log(choice);
+option.forEach((element)=>{
+  element.addEventListener(('click'),()=>{
+    option.forEach(o=>{
+      o.style.backgroundColor="rgb(0, 153, 255)";
+      o.classList.remove('selected');
+    })
+    element.style.backgroundColor="orange";
+    element.classList.add('selected');
+    console.log(element);
+    
+  })
 })
+
+submit.addEventListener(('click'),()=>{
+  let selected=document.querySelector(".selected");
+  let green=document.querySelector(".green");
+  selected.style.backgroundColor="red";
+  green.style.backgroundColor="green";
+  disable_btn();
+
+})
+
+
+next.addEventListener(('click'),()=>{
+  get().then((a)=>{
+    console.log(a);
+    let n;
+  
+  for(let i=0;i<4;i++){
+      if(a.results[i].type==="multiple"){
+        question.innerHTML="Q . "+a.results[i].question;
+      }
+
+      n=i;
+    }
+
+    option_sort(a.results[n].incorrect_answers[0],a.results[n].incorrect_answers[1],a.results[n].incorrect_answers[2],a.results[n].correct_answer,op1,op2,op3,op4,n);
+
+
+ })
+
+let selected=document.querySelector(".selected");
+let green=document.querySelector(".green");
+selected.style.backgroundColor="rgb(0, 153, 255)";
+green.style.backgroundColor="rgb(0, 153, 255)";
+
+
+})
+
+
+
+function disable_btn(){
+  for(let j of option){
+    j.disabled=true;
+  }
+}
+
+function option_sort(a,b,c,d,m,n,o,p,l){
+  let x=[a,b,c,d];
+  let y=[m,n,o,p];
+
+  for(let i=0;i<x.length;i++){
+    let j=Math.floor(Math.random()*(x.length-1));
+    [x[i],x[j]]=[x[j],x[i]];
+  }
+
+  for(let j=0;j<y.length;j++){
+    y[j].innerHTML=x[j];
+  }
+for(let i=0;i<y.length;i++){
+  if(y[i].innerHTML==d){
+    y[i].classList.add('green');
+  }
+}
+  console.log(x);
+}
