27d197e9e3629faf7e8c94cb7e2eda6c13c77a6d
dibyajit <dibyajitnandi7205@gmail.com>
2024-07-23 14:46:56 +0530

    add

---
+++ portfolio/script.js
@@ -0,0 +1,13 @@
+const observer = new IntersectionObserver((el)=>[
+    el.forEach((el)=>{
+        if(el.isIntersecting){
+            el.target.classList.add('show');
+        }
+        else{
+            el.target.classList.remove('show');
+        }
+    })
+])
+
+const hiddenElements=document.querySelectorAll(".box");
+hiddenElements.forEach((el)=> observer.observe(el));
