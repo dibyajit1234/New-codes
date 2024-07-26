883f43c902413422f456169117d6afcf6434e584
dibyajit <dibyajitnandi7205@gmail.com>
2024-07-26 15:18:11 +0530

    add

--- portfolio/script.js
+++ portfolio/script.js
@@ -3,11 +3,12 @@ const observer = new IntersectionObserver((el)=>[
         if(el.isIntersecting){
             el.target.classList.add('show');
         }
-        else{
-            el.target.classList.remove('show');
-        }
+        // else{
+        //     el.target.classList.remove('show');
+        // }
     })
 ])
 
 const hiddenElements=document.querySelectorAll(".box");
 hiddenElements.forEach((el)=> observer.observe(el));
+
