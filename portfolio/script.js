911a1fbe76d1b708375135542efd27122299ef25
dibyajit <dibyajitnandi7205@gmail.com>
2024-07-28 01:42:43 +0530

    add

--- portfolio/script.js
+++ portfolio/script.js
@@ -10,5 +10,25 @@ const observer = new IntersectionObserver((el)=>[
 ])
 
 const hiddenElements=document.querySelectorAll(".box");
+let darkmode=document.querySelector("#darkmode");
+const heading=document.querySelector("#heading");
+const body=document.querySelector("body");
+
 hiddenElements.forEach((el)=> observer.observe(el));
 
+
+let count=0;
+darkmode.addEventListener("click",()=>{
+    count++;
+    console.log(count);
+
+    if(count%2!=0){
+        //heading.style.backgroundColor = 'gray';
+         body.style.backgroundColor='#2E3436';
+         body.style.color='white';
+    }else{
+        body.style.backgroundColor = 'white';
+        body.style.color='black';
+    }
+
+})
