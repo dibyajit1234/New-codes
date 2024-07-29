e275beceb6c6bcf842d791df4228f8eac2b0b3d2
dibyajit <dibyajitnandi7205@gmail.com>
2024-07-29 23:40:48 +0530

    add

--- portfolio/script.js
+++ portfolio/script.js
@@ -3,9 +3,6 @@ const observer = new IntersectionObserver((el)=>[
         if(el.isIntersecting){
             el.target.classList.add('show');
         }
-        // else{
-        //     el.target.classList.remove('show');
-        // }
     })
 ])
 
@@ -15,38 +12,46 @@ const heading=document.querySelector("#heading");
 const body=document.querySelector("body");
 const man=document.querySelector(".new");
 const h1=document.querySelector("h1");
-const h2=document.querySelector("h2");
+const h2=document.querySelectorAll("h2");
 const first_a=document.querySelector("#first_a");
 const second_a =document.querySelector("#second_a");
 const picture=document.querySelector(".picture");
-
+const intro=document.querySelector("#intro");
+const h4=document.querySelectorAll("h4");
 hiddenElements.forEach((el)=> observer.observe(el));
 
 
 let count=0;
 darkmode.addEventListener("click",()=>{
     count++;
-    console.log(count);
 
     if(count%2!=0){
-        //heading.style.backgroundColor = 'gray';
          body.style.backgroundColor='#2E3436';
          body.style.color='white';
          man.style.backgroundColor="#2E3436";
          h1.style.color="#A800FF";
-         h2.style.color="#A800FF";
+         h2.forEach((el)=>{
+            el.style.color="#A800FF";
+         })
          first_a.style.color="white";
          second_a.style.color="white";
-         picture.style.boxShadow="1px 1px 105px white";
-        //  h1.style.textShadow="1px 1px 2px white";
+         picture.style.boxShadow="1px 1px 30px white";
+         h4.forEach((el)=>{
+            el.style.color="#A800FF";
+         })
     }else{
+        h4.forEach((el)=>{
+            el.style.color="rgb(69, 7, 150)";
+         })
+         h2.forEach((el)=>{
+            el.style.color="rgb(69, 7, 150)";
+         })
         body.style.backgroundColor = 'white';
         body.style.color='black';
          man.style.backgroundColor="white";
          h1.style.color="rgb(15, 1, 66)";
-         h2.style.color="rgb(69, 7, 150)";
          first_a.style.color="black";
          second_a.style.color="black";
+         picture.style.boxShadow="1px 1px 30px rgb(69, 7, 150)";
     }
-
 })
