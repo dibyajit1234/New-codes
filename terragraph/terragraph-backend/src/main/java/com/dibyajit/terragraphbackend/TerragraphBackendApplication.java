8077c5afee0d1ae1775d85fcef18117b79720cfb
dibyajit1234 <dibyajitnandi7205@gmail.com>
2026-04-08 16:39:24 +0530

    Clean initial commit (no secrets)

---
+++ terragraph/terragraph-backend/src/main/java/com/dibyajit/terragraphbackend/TerragraphBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dibyajit.terragraphbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerragraphBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TerragraphBackendApplication.class, args);
+    }
+
+}
---
+++ terragraph/terragraph-backend/src/test/java/com/dibyajit/terragraphbackend/TerragraphBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dibyajit.terragraphbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerragraphBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
